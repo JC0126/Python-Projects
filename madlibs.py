423b158dbb7bfe84fadbf2eff2d668e40dff29ef
Joseph Choe <73798063+JC0126@users.noreply.github.com>
2020-11-12 17:35:45 -0500

    Add files via upload

---
+++ madlibs.py
@@ -0,0 +1,19 @@
+"""Mad Libs Generator"""
+
+loop = 1
+while loop < 10:
+	noun1 = input("Choose a noun:    ")
+	rhyme1 = input("Choose a noun that rhymes with the noun you chose    ")
+	verb1 = input("Choose a verb     ")
+	rhyme2 = input("Choose a rhyme     ")
+
+
+
+
+
+	print("Boom! This is place crazy like your "+noun1)
+	print("Yeah! Blowing up your room like a "+rhyme1)
+	print("They call me lazy cuz I was just "+verb1)
+	print("It don't matter cuz I be "+rhyme2)
+	loop +=1
+
